d337907ce5a44627ce4fc1aa84e17fa5deb55d83
RagvDest <rennygorozabel@hotmail.com>
2020-12-09 14:21:18 -0500

    Actualizar

--- src/main/java/com/clearminds/smo/rgv/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/smo/rgv/servicios/RestEstudiantes.java
@@ -2,6 +2,7 @@ package com.clearminds.smo.rgv.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -21,6 +22,19 @@ public class RestEstudiantes {
 		ServicioEstudiante srvEst = new ServicioEstudiante();
 		try {
 			srvEst.insertarEstudiante(est);
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Path("/actualizar")
+	@PUT
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	public void actualizar(Estudiante est){
+		ServicioEstudiante srvEst = new ServicioEstudiante();
+		try {
+			srvEst.actualizarEstudiante(est);
 		} catch (BDDException e) {
 			e.printStackTrace();
 		}
