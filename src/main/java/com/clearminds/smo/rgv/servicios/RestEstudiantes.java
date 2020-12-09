789e7fb3152fa82b2bb302ef0f0608575b87e962
RagvDest <rennygorozabel@hotmail.com>
2020-12-09 12:09:44 -0500

    initial

---
+++ src/main/java/com/clearminds/smo/rgv/servicios/RestEstudiantes.java
@@ -0,0 +1,9 @@
+package com.clearminds.smo.rgv.servicios;
+
+import com.clearminds.rgv.dtos.Estudiante;
+
+public class RestEstudiantes {
+	public void insertar(Estudiante est){
+		System.out.println(est);
+	}
+}
